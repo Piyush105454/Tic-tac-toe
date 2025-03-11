3a46427b607a16382857959c224811368821784f
Piyush Tamoli <piyushmodi812@gmail.com>
2025-03-11 08:46:00 +0530

    Update js.js

---
+++ js.js
@@ -0,0 +1,111 @@
+let previousColor = ""; // Variable to store the previous color
+
+function change() {
+    let div1 = document.getElementById("div1");
+    let currentColor = window.getComputedStyle(div1).backgroundColor; // Get the current computed background color
+
+    // If previousColor is not set, we initialize it
+    if (previousColor === "") {
+        previousColor = currentColor; // Store the current color for the first time
+    }
+
+    // If the current color is blue, revert to the previous color
+    if (currentColor === "rgb(255, 255, 255)") { // 'rgb(0, 0, 255)' is the RGB representation of blue
+        div1.style.backgroundColor = previousColor; // Revert to the previous color
+    } else {
+        previousColor = currentColor; // Save the current color as the previous color
+        div1.style.backgroundColor = "#fff"; // Change the color to blue
+    }
+}
+
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector(".restart");
+let newGameBtn = document.querySelector(".new");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turnO = true; //playerX, playerO
+let count = 0; //To Track Draw
+
+const winPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+const resetGame = () => {
+  turnO = true;
+  count = 0;
+  enableBoxes();
+  msgContainer.classList.add("hide");
+};
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turnO) {
+      //playerO
+      box.innerText = "O";
+      turnO = false;
+    } else {
+      //playerX
+      box.innerText = "X";
+      turnO = true;
+    }
+    box.disabled = true;
+    count++;
+
+    let isWinner = checkWinner();
+
+    if (count === 9 && !isWinner) {
+      gameDraw();
+    }
+  });
+});
+
+const gameDraw = () => {
+  msg.innerText = `Game was a Draw.`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+
+const disableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
+
+const enableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+};
+
+const showWinner = (winner) => {
+  msg.innerText = `Congratulations, Winner is ${winner}`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+        showWinner(pos1Val);
+        return true;
+      }
+    }
+  }
+};
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
